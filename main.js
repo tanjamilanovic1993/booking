9ad3cd32b5027c5eb63e1018bdb96473f314e38f
tanjamilanovic1993 <tanjamilanovic1993@gmail.com>
2018-06-23 20:12:39 +0200

    Added MainButtons active state and Add/Sub functions for SubButtons

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function Ekran465(x) {
     }
 }
 $(document).ready(function () {
+
     var x = window.matchMedia("(max-width: 330px)");
     Ekran330(x);
     x.addListener(Ekran330);
@@ -27,27 +28,21 @@ $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $("#userb").click(function () {
-        $("#guestb").collapse('hide');
-    });
-    $("#guestb").click(function () {
-        $("#usertab").collapse('hide');
-    });
-
     $("#studiob").click(function () {
-        $("#FHbuttons").collapse('hide');
+        $("#Fbuttons").collapse('hide');
+        $("#Hbuttons").collapse('hide');
     });
 
     $("#flatb").click(function () {
-        $('#FHbuttons').collapse('show');
+        $("#Hbuttons").collapse('hide');
+        // $('#Fbuttons').collapse('show');
     });
 
     $("#houseb").click(function () {
-        $('#FHbuttons').collapse('show');
+        $("#Fbuttons").collapse('hide');
+        // $('#Hbuttons').collapse('show');
 
     });
-
-
 });
 
 function CarpetsHide() {
@@ -87,6 +82,49 @@ function OpenService2() {
 function OpenService3() {
     $("#nav-contact-tab").trigger("click");
 }
+
+// Active mainb
+$("#studiob, #flatb, #houseb, #noc, #hoov, #steamc").on("click", function () {
+    var btnklik = $(this);
+    btnklik.addClass('bactive');
+    btnklik.siblings().removeClass('bactive');
+});
+
+//Add counter
+$(".add").on("click", function () {
+    var  t = $(this);
+    t = t.siblings();
+    t = t[1];
+    var br = t.innerText;
+    var prop = br.substr(br.indexOf(' ')+1);//property name
+    br = br.substr(0,br.indexOf(' '));//counter
+    br++;
+    t.innerText = br + " " + prop;
+    console.log("Dodata soba " + br);
+});
+
+//Substract counter
+$(".sub").on("click", function () {
+    var  t = $(this);
+    t = t.siblings();
+    t = t[0];
+    var br = t.innerText;
+    var prop = br.substr(br.indexOf(' ')+1);//property name
+    br = br.substr(0,br.indexOf(' '));//counter
+    if( br == 0){
+        console.log("Cant go under 0!");
+    }
+    else{
+        br--;
+    }
+    if((br==0) && (prop=="Bedroom" || prop =="Bathroom" || prop == "Floor")){ //Bed, Bath and Floor cant be less then 1
+        br=1
+    }
+    t.innerText = br + " " + prop;
+    console.log("Oduzeta soba " + br);
+});
+
+
 
 
 
